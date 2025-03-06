345eca696826561a26c5beb6998eddf4912522f8
Mark Nguyen <mark.t.nguyen@outlook.com>
2025-03-05 20:06:12 -0600

    Add Badge component parameters
    - Added type and color parameters to the Badge component
    - Defaults to 'Badge' for type and 'gray' for color if no values are provided by the user

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -2,7 +2,7 @@ import clsx from "clsx"
 import {colors, getRandomColor} from "./utils/colors"
 import "./Badge.css"
 
-export default function Badge({children="Badge"}) {
+export default function Badge({children="Badge", type="square", color="gray"}) {
 
     return (
         <div 
