2d8773f12051ffd39b6141a6299b0fdeafdb2fdb
Mark Nguyen <mark.t.nguyen@outlook.com>
2025-03-05 18:53:10 -0600

    Add colors utility file with color exports

---
+++ src/components/utils/colors.js
@@ -0,0 +1,34 @@
+export const colors = [
+    {
+        backgroundColor: "#F3F4F6",
+        color: "#1F2937"
+    },
+    {
+        backgroundColor: "#FEE2E2",
+        color: "#991B1B"
+    },
+    {
+        backgroundColor: "#FEF3C7",
+        color: "#92400E"
+    },
+    {
+        backgroundColor: "#D1FAE5", 
+        color: "#065F46"
+    },
+    {
+        backgroundColor: "#DBEAFE",
+        color: "#1E40AF"
+    },
+    {
+        backgroundColor: "#E0E7FF",
+        color: "#3730A3"
+    },
+    {
+        color: "#5B21B6",
+        backgroundColor: "#EDE9FE"
+    },
+    {
+        backgroundColor: "#FCE7F3",
+        color: "#9D174D"
+    }
+]
