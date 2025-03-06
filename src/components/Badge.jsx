98852e7f7743255911a6af7f9451e87e5162ea0b
Mark Nguyen <mark.t.nguyen@outlook.com>
2025-03-05 20:04:41 -0600

    Import dependencies for Badge
    - Imported badge.css for styles
    - Imported colors data and getRandomColor function from colors.js

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,9 +1,12 @@
 import clsx from "clsx"
+import {colors, getRandomColor} from "./utils/colors"
+import "./Badge.css"
 
 export default function Badge({children="Badge"}) {
-       
+
     return (
-        <div className="badge">
+        <div 
+            className="badge">
             {children}
         </div>
     )
