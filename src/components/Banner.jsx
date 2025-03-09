1f436d7657a04a0e73d2de95822b02bbb8a6e5ed
Mark Nguyen <mark.t.nguyen@outlook.com>
2025-03-08 19:19:01 -0600

    Add and Export Banner components
    - Implemented Banner components: Banner, BannerTitle, and BannerParagraph.
    - Exported these components for use throughout the project.

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,3 +6,19 @@ import {
     faCircleInfo
 } from '@fortawesome/free-solid-svg-icons'
 import './Banner.css'
+
+export function Banner({children}) {
+    return (
+        <div className="banner">
+            {children}
+        </div>
+    )
+}
+
+export function BannerTitle({children}) {
+    return <p>{children}</p>
+}
+
+export function BannerParagraph({children}) {
+    return <p>{children}</p>
+}
