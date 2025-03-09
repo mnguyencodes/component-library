aeb335b33fe67156ab018fc410e98ec0761f5220
Mark Nguyen <mark.t.nguyen@outlook.com>
2025-03-08 18:56:28 -0600

    Import Font Awesome icons

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,2 +1,8 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { 
+    faCircleCheck,
+    faTriangleExclamation,
+    faCircleXmark,
+    faCircleInfo
+} from '@fortawesome/free-solid-svg-icons'
 
