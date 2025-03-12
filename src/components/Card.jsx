9e74a1cdb10036a12bb991b97a70197e8beee513
Mark Nguyen <mark.t.nguyen@outlook.com>
2025-03-11 19:00:35 -0600

    Add CardTitle and CardText components
    - Added components for CardTitle and CardText with filler code.
    - Updated Card component so that Card is no longer being exported as default.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,19 @@
 import "./Card.css"
 
-export default function Card() {
+export function Card() {
     return (
         <div className="card">Card Component Goes Here!</div>
+    )
+}
+
+export function CardTitle() {
+    return (
+        <div className="card-title">CardTitle Component Goes Here!</div>
+    )
+}
+
+export function CardText() {
+    return (
+        <div className="card-text">CardText Component Goes Here!</div>
     )
 }
